8ce3b6b3ed49f9d1ce1f65810ee9216ecd322a82
VitalisZ <33261095+VitalisZ@users.noreply.github.com>
2017-12-03 22:18:24 +0300

    Исправлены ошибки

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var link = document.querySelector(".booking-form-btn");
+var popup = document.querySelector(".hotels-search");
+
+var dataArrival = popup.querySelector("[name=arrival-date]");
+var dataDeparture = popup.querySelector("[name=departure-date]");
+var form = popup.querySelector("form");
+
+
+
+	link.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popup.classList.toggle("find-hotels");
+		dataArrival.focus();
+	});
+
+	form.addEventListener("submit", function (evt) {
+		if (!dataArrival.value || !dataDeparture.value) {
+			evt.preventDefault()
+			// popup.classList.remove("modal-error");
+			// popup.offsetWidth = popup.offsetWidth;
+			popup.classList.add("modal-error");
+			console.log("Укажите даты заезда и отъезда");
+		} else {
+
+		}
+	})
